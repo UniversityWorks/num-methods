d5ca77c069722673f03c875295d12d0efb500d87
Anton Popov <mffnrscl@gmail.com>
2025-03-24 10:31:23 +0200

    done lab1

--- lab1/code.cpp
+++ lab1/code.cpp
@@ -2,9 +2,9 @@
 #include <cmath>
 #include <iomanip>
 
-using namespace std;
 
-double f(double x) {
+double f(double x) 
+{
     return x * x - 2 * x - 4;
 }
 
@@ -16,9 +16,7 @@ double secantMethod(double x0, double x1, double epsilon, int &iterations) {
         x2 = x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0));
         iterations++;
         std::cout << "x" << iterations << " = " << x2 << std::endl; 
-        if (fabs(f(x2)) < epsilon) {
-            break;
-        }
+        if (fabs(f(x2)) < epsilon) break;
 
         x0 = x1;
         x1 = x2;
@@ -26,24 +24,26 @@ double secantMethod(double x0, double x1, double epsilon, int &iterations) {
     return x2;
 }
 
-int main() {
+int main() 
+{
     double h = 0.8;
     double epsilon = 0.01;
     int iterations;
 
-    cout << fixed << setprecision(5);
-    cout << "Значення функції на проміжку [-4; 4] з кроком " << h << ":\n";
+    std::cout << std::fixed << std::setprecision(5);
+    std::cout << "Значення функції на проміжку [-4; 4] з кроком " << h << ":\n";
 
-    for (double x = -4; x <= 4; x += h) {
-        cout << "f(" << x << ") = " << f(x) << endl;
+    for (double x = -4; x <= 4; x += h)
+    {
+        std::cout << "f(" << x << ") = " << f(x) << std::endl;
     }
 
-    cout << "\nМетод січних для знаходження кореня на проміжку [-1.6; -0.8]:\n";
+    std::cout << "\nМетод січних для знаходження кореня на проміжку [-1.6; -1.52]:\n";
 
-    double root = secantMethod(-0.8, -1.6, epsilon, iterations);
-    cout << "Знайдений корінь: x = " << root << endl;
-    cout << "f(x) = " << f(root) << endl;
-    cout << "Кількість ітерацій: " << iterations << endl;
+    double root = secantMethod(-1.6, -1.52, epsilon, iterations);
+    std::cout << "Знайдений корінь: x = " << root << std::endl;
+    std::cout << "f(x) = " << f(root) << std::endl;
+    std::cout << "Кількість ітерацій: " << iterations << std::endl;
 
     return 0;
 }
