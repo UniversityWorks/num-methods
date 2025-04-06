c0db947f1c5d3d236fe7b8de397fcd1f7c9cc176
Anton Popov <mffnrscl@gmail.com>
2025-04-06 16:18:11 +0300

    done lab1

--- lab1/code.cpp
+++ lab1/code.cpp
@@ -30,6 +30,11 @@ double secantMethod(double x0, double x1, double epsilon, int &iterations)
     return x2;
 }
 
+double second_root(double root1, double root2)
+{
+    return root1 - root2/10.0;
+}
+
 int main() 
 {
     double h = 0.8;
@@ -38,28 +43,52 @@ int main()
 
     std::cout << std::fixed << std::setprecision(5);
     std::cout << "Значення функції на проміжку [-4; 4] з кроком " << h << ":\n";
+    std::cout << "Знайдемо корені рівняння: " << std::endl;
 
-    for (double x = -4; x <= 4; x += h)
+    for (double x = -3.2; x <= 4; x += h)
     {
+        bool checker = true;
+
         std::cout << "f(" << x << ") = " << f(x) << std::endl;
+        if (f(x) < 0 && f(x-0.8) > 0 || f(x) > 0 && f(x-0.8) < 0 )
+        {
+            std::cout << "Знайдено Проміжок зміни знаку f(x): " << std::endl;
+            std::cout << "Уточнення коренів f'(x):" << std::endl;
+
+            for (double dx = x-0.8; dx <= x; dx+=0.2)
+            {
+                std::cout << "f'(" << dx << ") = " << df(dx) << std::endl;
+              
+                if(df(dx) < 0 && df(dx-0.2) > 0 || df(dx) > 0 && df(dx-0.2) < 0 )
+                {
+                    std::cout << "Зміна знаку в точках на проміжку." << std::endl;
+                    checker = false;
+                }
+                
+            }
+
+        }
+
+        if(checker && f(x) < 0 && f(x-0.8) > 0 || checker &&f(x) > 0 && f(x-0.8) < 0 )
+        {
+            std::cout << "корені x0 та x1 для методу січних: [" << x-0.8 << ';' <<  second_root(x-0.8, x) << "]." << std::endl;
+            std::cout << "\nМетод січних для знаходження кореня на проміжку [" << f(x-0.8) << ';' <<  second_root(x-0.8, x) << "]: " << std::endl;
+
+            double root = secantMethod(x-0.8, second_root(x-0.8, x), epsilon, iterations);
+            std::cout << "Знайдений корінь: x = " << root << std::endl;
+            std::cout << "f(x) = " << f(root) << std::endl;
+            std::cout << "Кількість ітерацій: " << iterations << std::endl;
+        
+        }
+        
     }
     
-    std::cout << "Проміжок зміни знаку f(x): [-1.6; -0.8]." << std::endl;
-    std::cout << "Уточнення коренів f'(x):" << std::endl;
 
-    for (double x = -1.6; x <= -0.6; x+=0.2)
-    {
-        std::cout << "f'(" << x << ") = " << df(x) << std::endl;
-    }
+   
       
     
 
-    std::cout << "\nМетод січних для знаходження кореня на проміжку [-1.6; -1.52]:\n";
 
-    double root = secantMethod(-1.6, -1.52, epsilon, iterations);
-    std::cout << "Знайдений корінь: x = " << root << std::endl;
-    std::cout << "f(x) = " << f(root) << std::endl;
-    std::cout << "Кількість ітерацій: " << iterations << std::endl;
 
     return 0;
 }
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,36 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 
-bool Solution(const std::vector<double>& x, const std::vector<double>& prev_x, double epsilon)
+int main()
 {
-    for (size_t i = 0; i < x.size(); i++)  {if (fabs(x[i] - prev_x[i]) >= epsilon) return false; }
-    return true;
-}
-
-int main() 
-{
-    const double epsilon = 0.01;
-    std::vector<double> x = {0, 0, 0}; 
-    std::vector<double> prev_x(3);
-    int iterations = 0;
-
-    std::cout << "Розв'язок методом простої ітерації:" << std::endl;
     
-    do {
-        prev_x = x;
-
-        
-        x[0] = (15 - prev_x[1] - prev_x[2]) / -2;
-        x[1] = (10 - prev_x[0] - prev_x[2]) / -2;
-        x[2] = (12 + prev_x[0] - 3 * prev_x[1]) / -6;
-
-        iterations++;
-        
-        std::cout << "Ітерація " << iterations << ": x1 = " << x[0] << ", x2 = " << x[1] << ", x3 = " << x[2] << std::endl;
-    } while (!Solution(x, prev_x, epsilon));
-
-    std::cout << "Знайдене рішення: x1 = " << x[0] << ", x2 = " << x[1] << ", x3 = " << x[2] << std::endl;
+    
     return 0;
 }
