fcd3505b6c2cc6abe6f9da0c16e0d6bb76ac20a7
Anton Popov <mffnrscl@gmail.com>
2025-05-12 11:53:47 +0300

    s

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,47 +1,77 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <iomanip>
 
-bool isConverged(std::vector<double>& x, std::vector<double>& prev_x, double epsilon)
+using namespace std;
+
+vector<double> multiplyMatrixVector(const vector<vector<double>>& matrix, const vector<double>& vec) 
 {
-
-    for (size_t i = 0; i < x.size(); ++i) if (fabs(x[i] - prev_x[i]) >= epsilon) return false;
-   
-    return true;
-
+    vector<double> result(matrix.size(), 0.0);
+  
+    for (size_t i = 0; i < matrix.size(); ++i) 
+    {
+        for (size_t j = 0; j < matrix[i].size(); ++j) result[i] += matrix[i][j] * vec[j];
+    }
+    return result;
 }
 
-std::vector<double> iterate(const std::vector<double>& prev_x)
+vector<double> addVectors(const vector<double>& a, const vector<double>& b)
+ {
+    vector<double> result(a.size());
+    for (size_t i = 0; i < a.size(); ++i)  result[i] = a[i] + b[i];
+    return result;
+}
+
+bool isConverged(const vector<double>& x, const vector<double>& prev_x, double epsilon) 
 {
-    std::vector<double> x(3);
-    x[0] = (15 - prev_x[1] - prev_x[2]) / -2;
-    x[1] = (10 - prev_x[0] - prev_x[2]) / -2;
-    x[2] = (12 + prev_x[0] - 3 * prev_x[1]) / -6;
+    for (size_t i = 0; i < x.size(); ++i) 
+    {
+        if (fabs(x[i] - prev_x[i]) >= epsilon) return false;
+    }
+    return true;
+}
+
+vector<double> solveBySimpleIteration(const vector<vector<double>>& iterMatrix, const vector<double>& constVector, double epsilon) {
+    size_t n = constVector.size();
+    vector<double> x(n, 0.0);     
+    vector<double> prev_x(n);
+    int iteration = 0;
+    
+    cout << "Розв'язок методом простої ітерації:" << endl;
+    
+    do {
+        prev_x = x;
+        vector<double> iterPart = multiplyMatrixVector(iterMatrix, prev_x);
+        x = addVectors(iterPart, constVector);
+        
+        iteration++;
+        
+        cout << "Ітерація " << iteration << ": ";
+        for (double val : x) {
+            cout << fixed << setprecision(5) << val << " ";
+        }
+        cout << endl;
+        
+    } while (!isConverged(x, prev_x, epsilon) && iteration < 100);
+    
     return x;
 }
 
 int main() {
-    const double epsilon = 0.01;
-    int iterations = 0;
-
-    std::vector<double> x = {0, 0, 0};
-    std::vector<double> prev_x(3);
-
-    std::cout << "Розв'язок методом простої ітерації:\n";
-
-    do {
-        prev_x = x;
-        x = iterate(prev_x);
-
-        iterations++;
-        std::cout << "Ітерація " << iterations << ": ";
-        for (double val : x) std::cout << val << " ";
-        std::cout << "\n";
-
-    } while (!isConverged(x, prev_x, epsilon));
-
-    std::cout << "\nЗнайдене рішення:\n";
-    for (int i = 0; i < x.size(); ++i) std::cout << "x" << i + 1 << " = " << x[i] << "\n";
-
+    
+    vector<vector<double>> iterMatrix = {
+        {0,    0.5,  0.5},
+        {0.5,  0,    0.5},
+        {-1.0/6, 0.5,  0}
+    };
+    
+    vector<double> constVector = {-7.5, -5, -2};
+    
+    vector<double> solution = solveBySimpleIteration(iterMatrix, constVector, 0.01);
+    
+    cout << "\nЗнайдене рішення:" << endl;
+    for (size_t i = 0; i < solution.size(); ++i) cout << "x" << i+1 << " = " << solution[i] << endl;
+    
     return 0;
 }
---
+++ lab4/code.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+
+
+void lagrange_interpolation_pol(std::vector<int> xi, std::vector<int> yi)
+{
+
+}
+int main()
+{
+
+    std::vector<int> xi{0,3,6,8,10};
+    std::vector<int> yi{5,2,0,4,6};
+
+    return 0;
+}
